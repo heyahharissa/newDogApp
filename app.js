e7626395e73bbf92ef4ffa937779239fe280e346
Alyssa Amrhein <alyssa@gooee.com>
2016-04-13 21:02:18 -0400

    added some firebase stuff really should finish that

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-console.log("oh shit check this shit outttt");
+// made an app
+var app = angular.module('app', ['firebase']);
 
-var app = angular.module('app', []);
-
+// just printing shib out to the page
 app.controller('MainCtrl', function ($scope) {
     $scope.name = "Alyssa";
 });
+
+// start to firebase
+app.controller("FireCtrl", function($scope, $firebaseObject) {
+    var ref = new Firebase("https://newdogapp.firebaseio.com");
+    // download the data into a local object
+    $scope.data = $firebaseObject(ref);
+    // putting a console.log here won't work, see below
+});
+
+// This was step 3
+// do more steps @ https://www.firebase.com/docs/web/libraries/angular/quickstart.html
